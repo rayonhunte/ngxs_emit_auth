0d9fe192d6bc691301c4549dd52eac26cd4a92f0
rayonhunte <rayon.hunte@gmail.com>
2018-11-29 09:35:09 -0400

    remove firebase info

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDzjPoPQopfej6PlWytMWwWM8NFrUEW3D8',
-    authDomain: 'zoon-366d8.firebaseapp.com',
-    databaseURL: 'https://zoon-366d8.firebaseio.com',
-    projectId: 'zoon-366d8',
-    storageBucket: 'zoon-366d8.appspot.com',
-    messagingSenderId: '347483141475'
+    // your firebase config
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDzjPoPQopfej6PlWytMWwWM8NFrUEW3D8',
-    authDomain: 'zoon-366d8.firebaseapp.com',
-    databaseURL: 'https://zoon-366d8.firebaseio.com',
-    projectId: 'zoon-366d8',
-    storageBucket: 'zoon-366d8.appspot.com',
-    messagingSenderId: '347483141475'
+     // your firebase config
   }
 };
 
